a7978a39066141cd8f63ebc22e6787a09bd7596a
KARTHICKRAJM84 <karthickrajm84@gmail.com>
2024-07-19 12:58:32 +0530

    first commit

---
+++ kar/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+  let name="karthi"
+  let age="20"
+  let activate=false;
+  let arr =[1,2,3,4,5,6,7]
+  let obj={
+    "name":"Name",
+    "phone":"9942490400"
+  } 
+let menus=["apple","banana","watermelon"]
+let output=[]
+for(let i=0;i<menus,length;i++)
+  return (
+    <>
+      
+      
+    </>
+  )
+}
+
+export default App
